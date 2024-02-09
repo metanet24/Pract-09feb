e24a2c04ee4dc0c712142e6efbe90612585f8350
unknown <matanatna@code.edu.az>
2024-02-09 23:16:47 +0400

    added

---
+++ practice/assets/js/script.js
@@ -0,0 +1,102 @@
+"use strict"
+
+let word = "Azerbaycan,salam,necesen";
+
+// console.log(word.length);
+
+// for (let i = 0; i < word.length; i++) {
+//       console.log(word[i]);
+// }
+
+// console.log(word.toLowerCase());
+
+// console.log(word.toUpperCase());
+
+// console.log(word.trim());
+
+// console.log(word.charCodeAt(5));
+
+// let res = word.concat(" salam")
+// console.log(res);
+
+// console.log(word.slice(0,4));
+// console.log(word.substring(1,4));
+// let text = "salam wsedrftghyujik"
+
+// console.log(text.substring(0,11)+"...");
+
+
+// let res = word.replace("A","a");
+
+// console.log(res);
+
+// let res = word.indexOf("z");
+// console.log(res);
+
+// console.log(word.lastIndexOf("a"));
+
+// console.log(word.replaceAll("a","b"));
+// console.log(word.replace("a","b"));
+
+// let text ="5";
+// text = text.padStart(4,"0")
+// console.log(text);
+
+// console.log(word.split(","));
+
+// console.log(word.startsWith("A"));
+
+// console.log(word.endsWith("m"));
+
+// let ages =[22,44,50,16];
+
+// // console.log(ages.length);
+
+// let res = ages.toString()
+// console.log(res);
+
+// console.log(ages.at(2));
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.join(" "));
+// fruits.pop()
+
+// fruits.push("test")
+
+// fruits.shift();
+// fruits.unshift("test")
+// console.log(fruits);
+
+
+// let res = fruits.filter(m=>m.includes("Orange"));
+// fruits=fruits.filter(m=>!m.includes("Orange"));
+// // console.log(res);
+// console.log(fruits);
+
+// fruits.splice(0,1,"lemon");
+// console.log(fruits);
+
+
+// console.log(fruits.includes("Banana"));
+
+// let res = fruits.find(m=>m.startsWith("B"))
+// console.log(res);
+
+// fruits.forEach((fruit,i) => {
+//     fruits[i] = fruit +" "+"P418"
+// });
+// console.log(fruits);
+
+// let res = fruits.map(m=>{
+//     return  m+" "+418
+// });
+
+// console.log(fruits);
+// console.log(res);
+
+
+
+
+
+
+
